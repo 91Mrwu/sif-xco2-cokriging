d8ce38dcaa1831a1918b3eaf2ca52e23b6592c6c
joshhjacobson <joshuah.jacobson@gmail.com>
2020-11-13 10:38:01 +1100

    Create land only xco2 dataset

--- src/data_utils.py
+++ src/data_utils.py
@@ -8,49 +8,54 @@ import xarray
 # ---------------------------------------------------
 def prep_sif(ds):
     """Preprocess an OCO-2 SIF Lite file"""
-    
+
     # Drop unused variables
-    variable_list = ["Daily_SIF_740nm", "SIF_Uncertainty_740nm", "Quality_Flag", "Longitude", "Latitude", "Delta_Time"]
+    variable_list = [
+        "Daily_SIF_740nm",
+        "SIF_Uncertainty_740nm",
+        "Quality_Flag",
+        "Longitude",
+        "Latitude",
+        "Delta_Time",
+    ]
     ds = ds[variable_list]
-    
+
     # Apply quality filters
-    ds["SIF_plus_3sig"] = ds.Daily_SIF_740nm + 3*ds.SIF_Uncertainty_740nm
+    ds["SIF_plus_3sig"] = ds.Daily_SIF_740nm + 3 * ds.SIF_Uncertainty_740nm
     ds = ds.where(ds.Quality_Flag != 2, drop=True)
     ds = ds.where(ds.SIF_plus_3sig > 0, drop=True)
 
     # Format dataset
     return xarray.Dataset(
-        {
-            "sif": (["time"], ds.Daily_SIF_740nm),
-        },
+        {"sif": (["time"], ds.Daily_SIF_740nm),},
         coords={
             "lon": (["time"], ds.Longitude),
             "lat": (["time"], ds.Latitude),
-            "time": ds.Delta_Time.values
-        }
+            "time": ds.Delta_Time.values,
+        },
     )
+
 
 def prep_xco2(ds):
     """Preprocess an OCO-2 FP Lite file"""
-    
+
     # Drop unused variables
     variable_list = ["xco2", "xco2_quality_flag", "longitude", "latitude", "time"]
     ds = ds[variable_list]
-    
+
     # Apply quality filters
     ds = ds.where(ds.xco2_quality_flag == 0, drop=True)
 
     # Format dataset
     return xarray.Dataset(
-        {
-            "xco2": (["time"], ds.xco2),
-        },
+        {"xco2": (["time"], ds.xco2),},
         coords={
             "lon": (["time"], ds.longitude),
             "lat": (["time"], ds.latitude),
-            "time": ds.time.values
-        }
+            "time": ds.time.values,
+        },
     )
+
 
 def read_transcom(path):
     """
@@ -59,7 +64,7 @@ def read_transcom(path):
     ds = xarray.open_dataset(path)
     ds = ds.where(ds.region < 12, drop=True)
     ds = ds.where(ds.region != 0, drop=True)
-    
+
     return ds
 
 
@@ -71,18 +76,19 @@ def regrid(ds, res=1):
     Convert dataset to dataframe and assign coordinates using a regular grid.
     """
     df = ds.to_dataframe()
-        
+
     # Establish grid
-    lon_bins = np.arange(-180, 180+res, res)
-    lat_bins = np.arange(-90, 90+res, res)
+    lon_bins = np.arange(-180, 180 + res, res)
+    lat_bins = np.arange(-90, 90 + res, res)
     lon_centers = (lon_bins[1:] + lon_bins[:-1]) / 2
     lat_centers = (lat_bins[1:] + lat_bins[:-1]) / 2
 
     # Overwrite lon-lat values with grid values
     df["lon"] = pd.cut(df.lon, lon_bins, labels=lon_centers).astype(float)
     df["lat"] = pd.cut(df.lat, lat_bins, labels=lat_centers).astype(float)
-    
+
     return df
+
 
 def map_transcom(ds, ds_tc):
     """
@@ -90,14 +96,15 @@ def map_transcom(ds, ds_tc):
     """
     # Regrid the dataset to 1-degree
     df_grid = regrid(ds, res=1).dropna().reset_index()
-    
+
     # Get transcom
     df_regions = ds_tc.to_dataframe().dropna().reset_index()
-    
+
     # Merge, format, return
-    return (df_grid
-            .merge(df_regions, on=["lon", "lat"], how="inner")
-            .drop(columns=["lon", "lat"])
-            .dropna()
-            .set_index(["time"])
-           )
+    return (
+        df_grid.merge(df_regions, on=["lon", "lat"], how="inner")
+        .drop(columns=["lon", "lat"])
+        .dropna()
+        .set_index(["time"])
+    )
+
--- src/stats.py
+++ src/stats.py
@@ -1,4 +1,8 @@
 # Statistical functions and wrappers
+import numpy as np
+import xarray
+
+
 def compute_xcov_1d(v1, v2, lag):
     """
     Empirical cross-covariance in 1-dimension
@@ -12,8 +16,9 @@ def compute_xcov_1d(v1, v2, lag):
     # truncate appropriate end of each vector to apply lag
     x = (v1 - np.nanmean(v1))[lag:]
     y = (v2 - np.nanmean(v2))[:-lag]
-    
-    return np.nanmean(x*y)
+
+    return np.nanmean(x * y)
+
 
 def compute_xcov_nd(Z1, Z2, lag):
     """
@@ -28,26 +33,27 @@ def compute_xcov_nd(Z1, Z2, lag):
     # apply mask for nan values
     Z1_m = np.ma.array(Z1, mask=np.isnan(Z1))
     Z2_m = np.ma.array(Z2, mask=np.isnan(Z2))
-    
+
     # truncate along time dim at appropriate position to apply lag
     X = (Z1_m - Z1_m.mean(axis=-1, keepdims=True))[:, :, lag:]
     Y = (Z2_m - Z2_m.mean(axis=-1, keepdims=True))[:, :, :-lag]
-    
+
     # compute cross-covariance along the time dimension
-    xcov = np.mean(X*Y, axis=-1)
-    
+    xcov = np.mean(X * Y, axis=-1)
+
     # return data values with missing entries filled as nan
     # NOTE: maybe use a special flag here instead
     return np.ma.filled(xcov.astype(float), np.nan)
-    
+
+
 def apply_cross_covariance(X, Y, lag=0):
     return xarray.apply_ufunc(
         compute_xcov_nd,
         X,
         Y,
-        kwargs = {"lag": lag},
-        input_core_dims = [["time"], ["time"]],
-        dask = "parallelized",
+        kwargs={"lag": lag},
+        input_core_dims=[["time"], ["time"]],
+        dask="parallelized",
         # vectorize = True, # needed if using 1-d cross-cov func
-        output_dtypes = [float],
+        output_dtypes=[float],
     )
